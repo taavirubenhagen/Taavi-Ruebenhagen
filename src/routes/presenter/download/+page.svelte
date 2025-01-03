165652c99cea0e4e3dd03e484003cadb8a9d5e94
Taavi Rübenhagen <t.ruebenhagen@gmail.com>
2025-01-03 22:30:47 +0100

    .

--- src/routes/presenter/download/+page.svelte
+++ src/routes/presenter/download/+page.svelte
@@ -23,7 +23,7 @@
     <Text medium paragraph>
         1. Click the button to download the file, then open it to start the installation (A MacOS version is coming soon):
         <br/><br/><br/>
-        <a download href="/presenter/Presentation Master 2.msi">
+        <a download href="/presenter/Presentation Master 2 - Desktop Companion.msi">
             <TextButton download onClick={() => {}}>
                 Download for Windows
             </TextButton>
@@ -36,8 +36,8 @@
         and I could therefore be made fully responsible for any security risks.<br/><br/>
         3. Refer to the <span class="text-blue-500 hover:text-blue-700"><InlineButton openInNew onClick={() => window.location.href = 'https://youtu.be/zjYiOR03C8M'}>video tutorial</InlineButton></span> if you don't know how to dismiss the warning.
         <span class="text-blue-500">If prompted, allow "Communication in private networks".</span> This is necessary since the communication between the devices uses WiFi.<br/><br/>
-        4. Whenever you want to present, open this app as well as your PowerPoint presentation, then use the mobile app to connect the devices and control the presentation.<br/><br/>
-        5. If you have any questions or concerns, feel free to contact me at t.ruebenhagen@gmail.com.<br/><br/>
+        4. Whenever you want to present, open this app and follow the instructions.
+        <br/><br/>
     </Text>
     <div class="absolute z-50 bottom-0 w-full h-24 flex justify-center items-center">
         <div class="w-[45%] flex justify-end">
