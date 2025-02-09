bee27dee88c27e5da6a315448855419df6e95f0a
Taavi Rübenhagen <t.ruebenhagen@gmail.com>
2025-02-09 18:35:54 +0100

    Untitled

--- src/routes/legal/imprint/+page.svelte
+++ src/routes/legal/imprint/+page.svelte
@@ -2,7 +2,7 @@
     import { Text } from "$lib/v1";
 </script>
 
-<main class="h-screen bg-white selection:bg-black px-4 sm:px-8 md:px-16 py-32 text-black selection:text-white font-mono">
+<main class="h-screen bg-white selection:bg-black px-4 sm:px-8 md:px-16 py-16 md:py-32 text-black selection:text-white font-mono">
     <Text medium heading>
         Imprint
         <br/><br/>
