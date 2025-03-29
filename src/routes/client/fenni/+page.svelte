407d98be2090ca41e8e759c02ed81df272cef6ff
Taavi Rübenhagen <t.ruebenhagen@gmail.com>
2025-03-29 11:18:36 +0100

    Untitled

--- src/routes/client/fenni/+page.svelte
+++ src/routes/client/fenni/+page.svelte
@@ -18,13 +18,25 @@
 />
 
 
-<main class="overflow-hidden min-h-screen flex flex-col justify-center items-center p-8 sm:p-16 text-center">
-    <Text large heading>
-        Hi, ich bin Fenni.
-    </Text>
-    <div class="h-16"></div>
-        <TextButton href="/presenter">
-            Meine Arbeit
-        </TextButton>
-    <Footer home title="GitHub" href="https://github.com/taavirubenhagen"/>
+<main class="overflow-hidden text-center text-white font-[Nunito]">
+    <img src="/fenni/wide_portrait.jpg" alt="Ich" class="fixed w-screen h-screen object-cover">
+    <div class="relative z-10 min-h-screen flex flex-col justify-center items-center p-8 sm:p-16">
+        <div class="text-6xl md:text-7xl font-bold" style="font-family: 'Permanent Marker';">
+            Hi, ich bin Fenni.
+        </div>
+        <div class="h-16"></div>
+        <div class="flex gap-4">
+            <a href="https://instagram.com/fennis.life" class="rounded-full bg-yellow-800 px-4 py-2 text-white font-bold">
+                <Text medium paragraph>
+                    Meine Arbeit
+                </Text>
+            </a>
+            <a href="mailto:contact@fenni.me" class="rounded-full bg-yellow-600 px-4 py-2 text-white font-bold">
+                <Text medium paragraph>
+                    Kontakt
+                </Text>
+            </a>
+        </div>
+        <Footer center home/>
+    </div>
 </main>
