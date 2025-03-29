86ab465010361ce49087fb95ff53fa247a64969e
Taavi Rübenhagen <t.ruebenhagen@gmail.com>
2025-03-29 10:28:17 +0100

    Untitled

---
+++ src/routes/client/fenni/+page.svelte
@@ -0,0 +1,35 @@
+<script lang='ts'>
+    import { Footer, Text, TextButton } from "$lib/v2";
+
+    let windowHeight = 1080;
+    let scrollY = 0;
+</script>
+
+
+<svelte:head>
+    <title>Home 𐄁 FENNI</title>
+    <meta name="description" content="FENNI 𐄁 Fenja Rübenhagen"/>
+</svelte:head>
+
+
+<svelte:window
+    bind:innerHeight={windowHeight}
+    bind:scrollY={scrollY}
+/>
+
+
+<main class="overflow-hidden min-h-screen flex flex-col justify-center p-8 sm:p-16">
+    <Text large heading>
+        Hi, ich bin Fenni.
+    </Text>
+    <div class="h-16"></div>
+    <div class="w-full flex justify-start gap-4">
+        <TextButton href="/presenter">
+            Explore
+        </TextButton>
+        <TextButton href="mailto:t.ruebenhagen@gmail.com">
+            Contact
+        </TextButton>
+    </div>
+    <Footer home title="GitHub" href="https://github.com/taavirubenhagen"/>
+</main>
