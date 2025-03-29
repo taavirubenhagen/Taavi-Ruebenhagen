9ab685b738cacf857675f7293c9792dd15318aa3
Taavi Rübenhagen <t.ruebenhagen@gmail.com>
2025-03-29 19:29:32 +0100

    Untitled

--- src/routes/portfolio/projects/+layout.svelte
+++ src/routes/portfolio/projects/+layout.svelte
@@ -4,7 +4,7 @@
 
 
 <main>
-    <div class="fixed z-40 top-0 w-full h-24 px-8 md:px-16 flex justify-between items-center">
+    <div class="fixed z-40 top-0 w-full h-24 px-8 md:px-16 pb-64 flex justify-between items-center">
         <a href="/" style="font-family: 'DM Mono';">
             <Text medium heading>
                 tr.
